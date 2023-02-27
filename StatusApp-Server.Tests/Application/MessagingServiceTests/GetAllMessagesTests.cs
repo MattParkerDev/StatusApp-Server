0cea51342fd0df9d45310d21969a6af64b946106
FakeRayBanz <matthewparker@hotmail.com.au>
2023-02-28 00:36:18 +1000

    Added MessagingService Test

---
+++ StatusApp-Server.Tests/Application/MessagingServiceTests/GetAllMessagesTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+using StatusApp_Server.Application;
+using StatusApp_Server.Application.Contracts;
+using StatusApp_Server.Domain;
+using StatusApp_Server.Infrastructure;
+using Xunit;
+
+namespace StatusApp_Server.Tests.Application.MessagingServiceTests;
+
+public class MessagingServiceTests
+{
+    [Fact]
+    public void WhenGetAllMessagesIsCalled_ReturnsMessageList()
+    {
+        //Arrange
+        var userName = "TestUserName";
+        var friendUserName = "AnotherUserName";
+        var groupId = Guid.NewGuid();
+
+        var messageList = new List<Message> { new Message { GroupId = groupId } };
+
+        var options = new DbContextOptions<ChatContext>();
+        var chatContextMock = new Mock<ChatContext>(options);
+        chatContextMock.Setup(db => db.Messages).ReturnsDbSet(messageList);
+
+        var messagingService = new MessagingService(chatContextMock.Object);
+
+        // Act
+        var result = messagingService.GetAllMessages(groupId);
+
+        // Assert
+        result.Should().BeEquivalentTo(messageList);
+    }
+}
