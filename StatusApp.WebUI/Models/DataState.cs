17b70af653fe33e1339f40969e24071551e7b7db
Matthew Parker <61717342+FakeRayBanz@users.noreply.github.com>
2023-03-30 01:03:36 +1000

    5 server webui handle getting friendslist (#6)
    5 Fix logged in state and clean up friend status view

--- StatusApp.WebUI/Models/DataState.cs
+++ StatusApp.WebUI/Models/DataState.cs
@@ -3,4 +3,15 @@
 public class DataState
 {
     public Profile? UserProfile { get; set; }
+    public bool Authorized { get; set; }
+    public IReadOnlyList<Profile>? FriendList { get; set; }
+    public event Action OnChange;
+
+    public void SetAuthorized(bool authorized)
+    {
+        Authorized = authorized;
+        NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
 }
