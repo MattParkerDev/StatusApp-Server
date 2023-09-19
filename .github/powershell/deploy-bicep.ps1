9adfa0f1e2d438733b10b8f1ee8ecd3504379bb6
Matthew Parker <matthewparker@hotmail.com.au>
2023-09-19 19:48:42 +1000

    Update deploy-bicep.ps1

--- .github/powershell/deploy-bicep.ps1
+++ .github/powershell/deploy-bicep.ps1
@@ -7,5 +7,5 @@ $kv_db_password = RetrieveSecretFromKeyVault("statusapp-database-admin-password"
 
 az deployment group create --resource-group "Statusapp" `
                            --template-file "../bicep/main.bicep" `
-                           --parameters "../bicep/main.parameters.json" "databaseAdminUsername=$kv_db_username" "databaseAdminPassword=$kv_db_password" `
+                           --parameters "../bicep/main.parameters.json" "database_admin_username=$kv_db_username" "database_admin_password=$kv_db_password" `
                            --mode Incremental
