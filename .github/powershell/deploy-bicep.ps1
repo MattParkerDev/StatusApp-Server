b26474219819e4744b6dd7591778766054374c92
Matthew Parker [SSW] <61717342+MattParkerDev@users.noreply.github.com>
2023-09-19 18:51:36 +1000

    cicd

--- .github/powershell/deploy-bicep.ps1
+++ .github/powershell/deploy-bicep.ps1
@@ -1,3 +1,4 @@
+. ($PSScriptRoot + "\functions.ps1") -Verbose # Import helper functions
 
 az account show
 
@@ -5,6 +6,6 @@ $kv_db_username = RetrieveSecretFromKeyVault("statusapp-database-admin-username"
 $kv_db_password = RetrieveSecretFromKeyVault("statusapp-database-admin-password")
 
 az deployment group create --resource-group "Statusapp" `
-                           --template-file "statusapp.bicep" `
-                           --parameters "main.parameters.json" "databaseAdminUsername=$kv_db_username" "databaseAdminPassword=$kv_db_password" `
+                           --template-file "../bicep/main.bicep" `
+                           --parameters "../bicep/main.parameters.json" "databaseAdminUsername=$kv_db_username" "databaseAdminPassword=$kv_db_password" `
                            --mode Incremental
